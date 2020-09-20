84342c854af500d3fbfb844cd875c81230aa9497
Gabriela Salvo <gabrielamdesalvo@gmail.com>
2020-09-20 16:17:14 -0300

    Implementando rotas e middlewares

--- src/index.js
+++ src/index.js
@@ -1,44 +1,75 @@
 const express = require('express');
 const { request } = require('express');
+const { uuid } = require('uuidv4');
+
 
 const app = express();
+app.use(express.json());
 
-/** 
- * GET: Buscar informações no back-end
- * POST: Criar uma informação no back-end
- * PUT/PATCH: Alterar uma informação no back-end
- * DELETE: Deletar uma informação no back-end
-*/
+const projects = [];
 
-/*
-Query Params: Filtros e paginação
-Route Params:
-Request Body:
-* */
+function logRequests(request, response, next) {
+    const { method, url } = request;
+
+    const logLabel =`[${method.toUpperCase()}] ${url}`;
+    
+    console.log(logLabel)
+
+    return next();
+
+}
+
+app.use(logRequests);
+
+
 app.get('/projects', (request, response)=>{
-    const { title, owner } = request.query;
-    console.log(title);
-    console.log(owner)
-    return response.json([
-        'Projeto 1',
-        'Projeto 2'
-    ])
+    const { title } = request.query;
+
+    const results = title ? projects.filter(project => project.title.includes(title)) : projects;
+
+    return response.json(results)
 });
 
 app.post('/projects',(request, response)=>{
-    return response.json([
-        'Projeto 3'
-    ])
+    const { title, owner }= request.body;
+    
+    const project = { id:uuid(), title, owner };
+
+    projects.push(project)
+
+    return response.json(project)
 })
 app.put('/projects/:id',(request, response)=>{
-    return response.json([
-        'Projeto 3'
-    ])
-})
+    const { id } = request.params;
+    const { title, owner }= request.body;
+
+    const projectIndex = projects.findIndex(project =>project.id === id);
+
+    if(projectIndex < 0){
+        return response.status(400).json({error: 'Project not found.'})
+    }
+    const project = {
+
+        id,
+        title,
+        owner
+    };
+    projects[projectIndex] = project;
+
+    return response.json(project);
+});
+
 app.delete('/projects/:id',(request, response)=>{
-    return response.json([
-        'Projeto 3'
-    ])
+
+    const { id } = request.params;
+    const projectIndex = projects.findIndex(project =>project.id === id);
+
+    if(projectIndex < 0){
+        return response.status(400).json({error: 'Project not found.'})
+    }
+    projects.splice(projectIndex, 1);
+  
+    return response.status(204).send();
 })
 
 
